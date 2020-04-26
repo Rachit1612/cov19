f50c867b5bcfe3314ac509de11f9439f84c1ad6a
Rachit1612 <rachitmishra1612@gmail.com>
2020-04-26 14:03:20 +0530

    First commit

---
+++ cov.js
@@ -0,0 +1,97 @@
+$(document).ready(function(){
+
+  // Get json data from URL
+  $.getJSON("https://api.covid19india.org/data.json",function(data){
+    var states = [];
+    var confirmed = [];
+    var recovered = [];
+    var deaths = [];
+    // console.log(data.statewise);
+    var total_confirmed;
+    var total_active;
+    var total_recovered;
+    var total_death;
+
+
+
+
+    $.each(data.statewise,function(id,obj){
+      states.push(obj.state);
+      confirmed.push(obj.confirmed);
+      recovered.push(obj.recovered);
+      deaths.push(obj.deaths);
+
+    })
+    // console.log(states) by shift one position;
+    states.shift();
+    confirmed.shift();
+    recovered.shift();
+    deaths.shift();
+    // console.log(states);
+    total_confirmed = data.statewise[0].confirmed;
+    total_active = data.statewise[0].active;
+    total_death = data.statewise[0].deaths;
+    total_recovered = data.statewise[0].recovered;
+
+    // console.log(total_confirmed);
+
+    $('#confirmed').append(total_confirmed);
+    $('#deaths').append(total_death);
+    $('#recovered').append(total_recovered);
+    $('#active').append(total_active);
+
+    var myChart = document.getElementById('myChart').getContext('2d');
+    var chart = new Chart(myChart,{
+      type:"bar",
+      data:{
+        labels:states,
+        datasets:[{
+          label:"Confirmed",
+          data:confirmed,
+          backgroundColor:"#9AC00D"
+        },
+        {
+          label:"Recovered",
+          data:recovered,
+          backgroundColor:"#FF5733"
+        },
+        {
+        label:"Deaths",
+        data:deaths,
+        backgroundColor:"#DAF7A6"
+      }
+        ]
+      },
+      options:{
+
+        legend: {
+          labels: {
+         fontColor: '#ffffff'
+      },
+           fontColor: "white",
+       },
+       scales: {
+           xAxes: [{
+               ticks: {
+                   fontColor: "white",
+                   stepSize: 5,
+                   beginAtZero: true
+               }
+           }],
+           yAxes: [{
+               ticks: {
+                   fontColor: "white",
+                   stepSize: 400,
+                   beginAtZero: true
+               }
+           }]
+       }
+
+
+      }
+    })
+
+
+  })
+
+})
---
+++ cov2.js
@@ -0,0 +1,74 @@
+$("#drop").ready(function(){
+
+  var drop = $("#drop");
+  drop.empty();
+
+  $.getJSON("https://api.covid19india.org/data.json",function(data){
+
+    var state = data.statewise;
+    var state_list=[];
+    var state_deaths=[];
+    var state_confirmed=[];
+    var state_recovered=[];
+    var last_updated=[];
+
+    $.each(state,function(id,obj){
+
+      state_list.push(obj.state);
+      state_deaths.push(obj.deaths);
+      state_recovered.push(obj.recovered);
+      state_confirmed.push(obj.confirmed);
+      last_updated.push(obj.lastupdatedtime)
+
+      drop.append("<option value=\""+id+"\">"+obj.state+"</option>")
+
+
+    })
+
+
+    $("#qq").click(function(){
+      var dd = $("#drop");
+      var idx = dd.val();
+      // console.log(idx);
+      // console.log(state_list[idx]);
+      $('h1').text("***"+state_list[idx]+"***");
+      $('.fo').fadeOut('fast');
+      $('#refresh').append("\t<a href=\"home_cov.html\">HOME</a> <br> ")
+      $('#refresh').append("<a href=\"state_cov.html\">Check Other State</a> <br>")
+      $('#updated').append("{ Last Updated On: "+last_updated[idx]+" }")
+      $('#bottom_note').append("*Click on the Confirmed/Recovered/Deaths buttons above to see the chart individually.*")
+
+
+      var myC = document.getElementById("myChart2").getContext('2d');
+      var chart2 = new Chart(myC,{
+        type:"pie",
+        data:{
+          labels: ['Confirmed','Deaths','Recovered'],
+          datasets:[{
+            label:"Info",
+            data:[state_confirmed[idx],state_deaths[idx],state_recovered[idx]],
+            backgroundColor:[
+                '#9AC00D',
+                '#DAF7A6',
+                '#FF5733'
+            ]
+          }]
+        },
+        options: {
+          legend: {
+            labels: {
+           fontColor: '#ffffff'
+        },
+      },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+      })
+    })
+  })
+})
